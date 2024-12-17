66a43d834afbd71ba15108e111ed151247723f51
Nikita Hazukin <nikita.hazukin@eventings.de>
2024-12-17 13:17:39 +0100

    test1

---
+++ src/scripts/login.js
@@ -0,0 +1,15 @@
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); 
+
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    if (username === "" || password === "") {
+      alert("Bitte Benutzername und Passwort eingeben!");
+    } else {
+      alert("Login erfolgreich! Du wirst weitergeleitet...");
+      window.location.href = "../pages/Main.html";
+    }
+  });
